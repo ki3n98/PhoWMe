7021e352f47cf685192dae0364452fbc583642e6
Kien Pham <ki3n98@gmail.com>
2023-07-24 16:43:46 -0700

    create a carousel

--- js/index2.js
+++ js/index2.js
@@ -1,14 +1,21 @@
-$("#cover-photo").animate({opacity: '1'}, "slow"); 
-
 /*----------Carosel----------*/
-var currentSlide = 1; 
 
-showSlide(0); 
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
-function showSlide(currentSlide) {
-    var slides = $(".slides"); 
+buttons.forEach(button => {
+    button.addEventListener("click", ()=>{
+        const offset = button.dataset.carouselButton ==="next"? 1:-1; 
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]"); 
 
-    slides[currentSlide].style.display = "block";  
-    
-}
+        const activeSlide = slides.querySelector("[data-active]"); 
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset; 
 
+        if (newIndex < 0)
+            newIndex = slides.children.length -1; 
+        if (newIndex >= slides.children.length)
+            newIndex = 0; 
+
+        slides.children[newIndex].dataset.active = true; 
+        delete activeSlide.dataset.active; 
+    })
+})
